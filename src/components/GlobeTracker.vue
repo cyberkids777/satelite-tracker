0942c4dd63d15a13425ed1e79b7dc645e0abac8c
cyberkids777 <niko.szotek@gmail.com>
2026-03-16 17:04:39 +0100

    chore: GlobeTracker component

---
+++ src/components/GlobeTracker.vue
@@ -0,0 +1,34 @@
+<script setup lang="js">
+import { ref, onMounted } from 'vue'
+import Globe from 'globe.gl'
+
+// Referencja do kontenera div, w którym wyrenderuje się canvas 3D
+const globeContainer = ref<HTMLElement | null>(null)
+
+onMounted(() => {
+  if (!globeContainer.value) return
+
+  // Inicjalizacja Globe.gl
+  const world =  Globe()(globeContainer.value)
+    .globeImageUrl('https://unpkg.com/three-globe/example/img/earth-blue-marble.jpg')
+    .bumpImageUrl('https://unpkg.com/three-globe/example/img/earth-topology.png')
+    .backgroundImageUrl('https://unpkg.com/three-globe/example/img/night-sky.png')
+
+  // Lekki auto-obrót planety
+  world.controls().autoRotate = true
+  world.controls().autoRotateSpeed = 0.5
+})
+</script>
+
+<template>
+  <div ref="globeContainer" class="globe-wrapper"></div>
+</template>
+
+<style scoped>
+.globe-wrapper {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  background-color: #000;
+}
+</style>
